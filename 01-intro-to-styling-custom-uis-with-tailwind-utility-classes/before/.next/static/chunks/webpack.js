e3be2db57aa3e17149ecc1844297af3a241f8e87
Unleash Hale <haleonearth@users.noreply.github.com>
2022-05-14 11:25:34 -0500

    finished flex columns scroll video

--- 01-intro-to-styling-custom-uis-with-tailwind-utility-classes/before/.next/static/chunks/webpack.js
+++ 01-intro-to-styling-custom-uis-with-tailwind-utility-classes/before/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "29717555d6a53934"; }
+/******/ 		__webpack_require__.h = function() { return "97e8c6a1faa174bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- 01-intro-to-styling-custom-uis-with-tailwind-utility-classes/before/pages/index.js
+++ 01-intro-to-styling-custom-uis-with-tailwind-utility-classes/before/pages/index.js
@@ -23,8 +23,12 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col flex-1 bg-gray-600">
-        <div className="px-3 shadow-md">General</div>
-        <div className="flex-1 p-4 bg-black">Messages</div>
+        <div className="flex items-center h-12 px-3 shadow-md">General</div>
+        <div className="flex-1 p-3 space-y-4 overflow-y-scroll">
+          {[...Array(40)].map((_, i) => (
+            <p>channel {i}. But I must explain to you how all this mistaken idea of denouncing of a pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
+          ))}
+        </div>
       </div>
     </div>
   );
